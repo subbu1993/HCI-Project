ecf442270f5d490ee2c30ec219d8fd7d5466d317
Subramaniam Swamynathan <ramani1993@gmail.com>
2015-09-17 23:31:52 -0400

    got the controller working

--- kiosk_controller.js
+++ kiosk_controller.js
@@ -1,4 +1,8 @@
 var kiosk = app.module('kiosk',[]);
 kiosk.controller('kioskController',function($scope){
+  console.log('controller called');
   $scope.text1 = "Find a class";
+  $scope.clickable = "Welcome to CampusBot";
+  $scope.class_button = false;
+  $scope.text1 = "blah";
 });
